9be9227b0ba5de18fd3c1af0c4fe85a236a54726
sugyan <sugi1982@gmail.com>
2024-09-01 14:08:09 +0900

    Add project

---
+++ main.ts
@@ -0,0 +1,73 @@
+import { Hono } from "https://deno.land/x/hono@v4.3.11/mod.ts";
+import { html } from "https://deno.land/x/hono@v4.3.11/helper/html/index.ts";
+import { WasmOAuthClient } from "./pkg/oauth_wasm.js";
+
+const URL_BASE = Deno.env.get("URL_BASE") || "http://localhost";
+const CALLBACK_PATH = "/callback";
+const CLIENT_METADATA_PATH = "/client-metadata.json";
+const JWKS_PATH = "/.well-known/jwks.json";
+
+const client = new WasmOAuthClient({
+  metadata: {
+    client_id: URL_BASE + CLIENT_METADATA_PATH,
+    client_uri: URL_BASE,
+    redirect_uris: [URL_BASE + CALLBACK_PATH],
+    token_endpoint_auth_method: "private_key_jwt",
+    grant_types: ["authorization_code"],
+    scopes: ["atproto"],
+    jwks_uri: URL_BASE + JWKS_PATH,
+    token_endpoint_auth_signing_alg: "ES256",
+  },
+  keys: [Deno.env.get("PRIVATE_KEY_1")],
+});
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.html(html`<!DOCTYPE html>
+    <html>
+      <h2>
+        ATProto OAuth with
+        <a href="https://github.com/sugyan/atrium/pull/219">ATrium</a>
+      </h2>
+      <p>Running <code>atrium-oauth-client</code> on Deno Deploy with WASM.</p>
+      <h2>Sign In</h2>
+      <form action="/signin">
+        <input type="text" name="input" placeholder="Your Handle" />
+        <button type="submit">Sign In</button>
+      </form>
+      <br />
+      or
+      <a href="/signin">Sign In with <code>bsky.social</code></a>
+      <h2>Resources</h2>
+      <ul>
+        <li><a href="/client-metadata.json">Client Metadata</a></li>
+        <li><a href="/.well-known/jwks.json">JWKS</a></li>
+      </ul>
+    </html>`);
+});
+app.get("/signin", async (c) => {
+  const input = c.req.query("input") || "https://bsky.social";
+  try {
+    return c.redirect(await client.authorize(input));
+  } catch (e) {
+    return c.json({ error: e });
+  }
+});
+app.get(CALLBACK_PATH, async (c) => {
+  const query = c.req.query();
+  try {
+    const tokenSet = await client.callback(query);
+    return c.json(tokenSet);
+  } catch (e) {
+    return c.json({ error: e });
+  }
+});
+app.get(CLIENT_METADATA_PATH, (c) => {
+  return c.json(client.client_metadata());
+});
+app.get(JWKS_PATH, (c) => {
+  return c.json(client.jwks());
+});
+
+Deno.serve({ port: 8765 }, app.fetch);
---
+++ src/lib.rs
@@ -0,0 +1,87 @@
+use atrium_oauth_client::store::state::MemoryStateStore;
+use atrium_oauth_client::{
+    AtprotoClientMetadata, OAuthClient, OAuthClientConfig, OAuthResolverConfig,
+};
+use elliptic_curve::pkcs8::DecodePrivateKey;
+use elliptic_curve::SecretKey;
+use jose_jwk::{Class, Jwk, Key, Parameters};
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::Serializer;
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsValue;
+
+#[derive(Serialize, Deserialize)]
+struct WasmOAuthClientConfig {
+    metadata: AtprotoClientMetadata,
+    keys: Option<Vec<String>>,
+}
+
+#[wasm_bindgen]
+pub struct WasmOAuthClient {
+    inner: OAuthClient<MemoryStateStore>,
+}
+
+#[wasm_bindgen]
+impl WasmOAuthClient {
+    #[wasm_bindgen(constructor)]
+    pub fn new(config_obj: JsValue) -> Result<WasmOAuthClient, JsValue> {
+        let config = serde_wasm_bindgen::from_value::<WasmOAuthClientConfig>(config_obj)?;
+        let keys = if let Some(keys) = config.keys {
+            let mut jwks = Vec::with_capacity(keys.len());
+            for (i, s) in keys.into_iter().enumerate() {
+                let Ok(secret_key) = SecretKey::<p256::NistP256>::from_pkcs8_pem(&s) else {
+                    return Err(JsValue::from_str("failed to parse secret key"));
+                };
+                jwks.push(Jwk {
+                    key: Key::from(&secret_key.into()),
+                    prm: Parameters {
+                        kid: Some(format!("key-{i:02}")),
+                        cls: Some(Class::Signing),
+                        ..Default::default()
+                    },
+                });
+            }
+            Some(jwks)
+        } else {
+            None
+        };
+        let client = OAuthClient::new(OAuthClientConfig {
+            client_metadata: config.metadata,
+            keys,
+            resolver: OAuthResolverConfig {
+                handle_resolver: "https://public.api.bsky.app".try_into().unwrap(),
+                plc_directory_url: None,
+            },
+            state_store: MemoryStateStore::default(),
+        })
+        .map_err(|e| JsValue::from_str(&e.to_string()))?;
+        Ok(Self { inner: client })
+    }
+    pub fn client_metadata(&self) -> Result<JsValue, JsValue> {
+        Self::to_json(&self.inner.client_metadata)
+    }
+    pub fn jwks(&self) -> Result<JsValue, JsValue> {
+        Self::to_json(&self.inner.jwks())
+    }
+    pub async fn authorize(&self, input: String) -> Result<String, JsValue> {
+        self.inner
+            .authorize(input, Default::default())
+            .await
+            .map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+    pub async fn callback(&self, params: JsValue) -> Result<JsValue, JsValue> {
+        Self::to_json(
+            &self
+                .inner
+                .callback(serde_wasm_bindgen::from_value(params)?)
+                .await
+                .map_err(|e| JsValue::from_str(&e.to_string()))?,
+        )
+    }
+    fn to_json<T>(value: &T) -> Result<JsValue, JsValue>
+    where
+        T: Serialize + ?Sized,
+    {
+        Ok(value.serialize(&Serializer::json_compatible())?)
+    }
+}
